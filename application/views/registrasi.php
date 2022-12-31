a356c4b7e9dba104a0ee862c29ca61f73f5dbddf
ademaulana12 <ademaulana464@gmail.com>
2022-12-31 22:38:24 +0700

    Update 31/12/2022

--- application/views/registrasi.php
+++ application/views/registrasi.php
@@ -114,9 +114,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 					<h5 class="modal-title">Yuk Login....</h5>
 				</div>
 				<div class="modal-body">
-					<form action="#" method="post">
+					<form action="<?= base_url('registrasi/login') ?>" method="post">
 						<div class="input-group mb-3">
-							<input type="email" class="form-control" placeholder="Email">
+							<input type="email" name="email" class="form-control" placeholder="Email">
 							<div class="input-group-append">
 								<div class="input-group-text">
 									<span class="fas fa-envelope"></span>
@@ -124,7 +124,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 							</div>
 						</div>
 						<div class="input-group mb-3">
-							<input type="password" class="form-control" placeholder="Password">
+							<input type="password" name="password" class="form-control" placeholder="Password">
 							<div class="input-group-append">
 								<div class="input-group-text">
 									<span class="fas fa-lock"></span>
